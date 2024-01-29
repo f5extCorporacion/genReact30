89424dc197510887e4be969e9401ab18c29d292d
Colombiantechnology <1107050440f@gmail.com>
2024-01-29 14:07:18 -0600

    insertando nuevos fondos y audio con diseño mas bonito

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -1,35 +1,43 @@
 import './../App.css'
 import quotes from'./../db/quotes.json';
-import lemale from'./../assets/img/lemale.png';
+import rojo from'./../assets/img/rojo.png';
 import { Ramdon } from '../utils/randon.js';
-
-
-
+import {Howl, Howler} from 'howler';
+import buttonClick from './../assets/sonidos/buttonClick.mp3';
+import cartoon from './../assets/sonidos/cartoon.mp3';
 
 const Frase = ({quote, setFranse}) => {
-    
 
+  var sound = new Howl({
+    src: [cartoon]
+  });
+
+  /**Evento del click */
     const handlera =()=>{
         setFranse(Ramdon(quotes));
+          sound.play()
+          Howler.volume(0.9);
+        
       };
+  
       
   return (
     <div className='ap'>
          <div className="lemale">
-               <img src={lemale} alt="" />
+               <img src={rojo} alt="" />
           </div>
-          
+          <br />
           <div className="frase">
             <div className="item">
-              <h3>Frases <br /> </h3>
-              <p> {quote.phrase}</p>
+              <h3>Frases <br /> </h3> <hr /><br />
+              <p> {quote.phrase}</p><br />
               <h5> Author: {quote.author} </h5>
               </div>
-          </div>
+          </div><br />
           <button onClick={handlera}> <h3>Siguiente</h3> </button>
-          <h1>Estudiante Academlo</h1>
-          <h4>Franklim de jesus muñoz valverde</h4>
+          
     </div>
   )
+ 
 }
 export default Frase;
